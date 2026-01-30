2d09aa0b407db79db2b0cb07b3b7f12d1ae25751
sumitbasak100 <91269228+sumitbasak100@users.noreply.github.com>
2026-01-30 12:49:30 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -84,6 +84,15 @@ export default function RootLayout({
         {children}
         <Analytics />
       </body>
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-8NZNWPD64D"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-8NZNWPD64D');
+      </script>
     </html>
   )
 }
